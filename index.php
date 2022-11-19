583e5c065e14afcebf75bc07544c46e6c68023a0
Noé Fmld <94837185+Noe-Fmld@users.noreply.github.com>
2022-11-19 18:36:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -137,3 +137,81 @@
                 $errorMsg = 'Résultat trop grand !';
         }
     }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculatrice chiffre romain</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</head>
+<body class="bg-secondary">
+
+    <section class="vh-100" style="background-color: #eee;">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-lg-9 col-xl-7">
+              <div class="card rounded-3">
+                <div class="card-body p-4">
+                  <h4 class="text-center my-3">Bienvenue sur une calculatrice de chiffre romain !</h4>
+                  <h6 class="text-center my-3" id="maj">Seules les lettres I, V, X, L, C, D et M sont autorisées.</h6>
+                  <div class="container-fluid">
+                    <div class="row d-inline justify-content-center">
+                        <form name="form" action="" method="post">
+                            <div>
+                                <label for="chr1">Premier chiffre romain :</label>
+
+                                <input type="text" id="chr1" name="chr1" required>
+                            </div>
+                            <div>
+                                <label for="chr2">Second chiffre romain :</label>
+
+                                <input type="text" id="chr2" name="chr2" required>
+                            </div>
+                            <div>
+                                <br>
+                                <button id="valider" class="btn btn-primary" type="submit">Calculer</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="row mt-5">
+                        <?php if ($errorLetters) : ?>
+                            <p>La lettre saisie n'existe pas ! (
+                                <?php foreach ($errorLetters as $error) : ?>
+                                    <?= $error ?>
+                                <?php endforeach ?>
+                                )
+                            </p>
+                        <?php elseif ($errorMsg) : ?>
+                            <p><?= $errorMsg ?></p>
+                        <?php elseif ($resultCh) : ?>
+<!--                            <p>Ch 1 : --><?//= $resultCh1 ?><!--</p>-->
+<!--                            <hr>-->
+<!--                            <p>Ch 2 : --><?//= $resultCh2 ?><!--</p>-->
+<!--                            <hr>-->
+                            <p>Le résultat est : <?= $resultRom ?> (<?= $resultCh ?>)</p>
+                        <?php endif ?>
+                    </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/locale/fr.min.js" integrity="sha512-RAt2+PIRwJiyjWpzvvhKAG2LEdPpQhTgWfbEkFDCo8wC4rFYh5GQzJBVIFDswwaEDEYX16GEE/4fpeDNr7OIZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="main.js"></script>
+</body>
+</html>
